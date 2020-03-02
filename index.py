bc0e6693d8211be1851c64aaa97e5cb9dfac273d
marut-muengree <61387885+marut-muengree@users.noreply.github.com>
2020-03-02 18:27:40 +0700

    new

---
+++ index.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar  2 17:59:41 2020
+
+@author: Marut
+"""
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
